bc4aab8fa1a574bfa2b491d2c59d56c82bb774da
sz0juhade <sz0juhade@vasvari.org>
2021-09-16 12:11:38 +0200

    Projekt létrejött.

---
+++ SpectatorProject/SpectatorProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpectatorProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
