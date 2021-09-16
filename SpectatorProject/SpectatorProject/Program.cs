94a1d27bcc0a719730126026f6e2cac1b88d05ce
sz0juhade <sz0juhade@vasvari.org>
2021-09-16 12:28:48 +0200

    Status elkészült.

--- SpectatorProject/SpectatorProject/Program.cs
+++ SpectatorProject/SpectatorProject/Program.cs
@@ -4,6 +4,18 @@ namespace SpectatorProject
 {
     class Program
     {
+        /// <summary>
+        /// Elérhetőség
+        /// </summary>
+        enum Status
+        {
+            Inactive,
+            Active,
+            Expired
+        }
+        /// <summary>
+        /// Név osztály
+        /// </summary>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
