f002b730b49a4669376d4c9d087664ef8ea005c3
sz0racnik <sz0racnik@vasvari.org>
2021-09-21 11:10:53 +0200

    hitelkártya osztály

--- SpectatorProject/SpectatorProject/Program.cs
+++ SpectatorProject/SpectatorProject/Program.cs
@@ -35,9 +35,77 @@ namespace SpectatorProject
             public string LastName { get => lastName; set => lastName = value; }
 
         }
+
+        /// <summary>
+        /// Hitelkártya osztály
+        /// </summary>
+        class Creditcard
+        {
+            /// <summary>
+            /// rejtett adattagok
+            /// </summary>
+            private DateTime expiredDate;
+            private string cardNumber;
+
+            /// <summary>
+            /// konstruktorok
+            /// </summary>
+            /// <param name="expiredDate"></param>
+            /// <param name="cardNumber"></param>
+            public Creditcard(string cardNumber, DateTime expiredDate)
+            {
+                this.ExpiredDate = expiredDate;
+                this.CardNumber = cardNumber;
+            }
+
+            public DateTime ExpiredDate { get => expiredDate; set => expiredDate = value; }
+            public string CardNumber { get => cardNumber; set => cardNumber = value; }
+
+            /// <summary>
+            /// Kártya lejárásának vizsgálata
+            /// </summary>
+            public bool Expired
+            {
+                get
+                {
+                    //ha lejárt
+                    if (expiredDate < DateTime.Now)
+                    {
+                        return true;
+                    }
+                    //ha nem járt le
+                    else
+                    {
+                        return false;
+                    }
+
+                }
+            }
+
+            public override string ToString()
+            {
+                string result = "Hitelkártya adatai: " + cardNumber;
+                result += "\nLejárati dátum: " + expiredDate;
+
+                if (Expired)
+                {
+                    result += "\nA kártya lejárt.";
+                }
+                else
+                {
+                    result += "\nA kártya nem járt le.";
+                }
+                return result;
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+
+
     }
+
+
 }
